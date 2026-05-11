71f090aa0f59ff510e9c4b0db1951de3fc8c4094
Dipali Dharmik <dharmikdipali9@gmail.com>
2026-05-11 17:54:09 +0530

    Settings.py updated

--- backend/interview_ai/interview_ai/settings.py
+++ backend/interview_ai/interview_ai/settings.py
@@ -86,10 +86,11 @@ WSGI_APPLICATION = 'interview_ai.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL')
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
     )
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
