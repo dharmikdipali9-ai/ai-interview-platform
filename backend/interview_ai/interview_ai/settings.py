1b4515813d5ac94fd7e1655d1a970feb35b83d73
Dipali Dharmik <dharmikdipali9@gmail.com>
2026-05-12 10:58:29 +0530

    setup production api url

--- backend/interview_ai/interview_ai/settings.py
+++ backend/interview_ai/interview_ai/settings.py
@@ -85,8 +85,8 @@ WSGI_APPLICATION = 'interview_ai.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.parse(
-        default=os.environ.get('DATABASE_URL'),
+    "default": dj_database_url.parse(
+        os.environ.get("DATABASE_URL"),
         conn_max_age=600,
         ssl_require=True
     )
