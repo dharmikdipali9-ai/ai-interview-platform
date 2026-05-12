dd0c76b48adfa304ce92a5d8b04de52629b3bd3c
Dipali Dharmik <dharmikdipali9@gmail.com>
2026-05-12 11:09:55 +0530

    updated base url

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://ai-interview-platform-yd7c.onrender.com/",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Attach token
