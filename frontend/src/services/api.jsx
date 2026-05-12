92f7990c6b595236a2f60ad12def2c9fda8ffda2
Dipali Dharmik <dharmikdipali9@gmail.com>
2026-05-12 10:42:26 +0530

    disable CI warnings

--- frontend/src/services/api.jsx
+++ frontend/src/services/api.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api/",
+  baseURL: "https://ai-interview-platform-yd7c.onrender.com/",
 });
 
 // Attach token
